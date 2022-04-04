f68bfff39c9aff49a938c2b3e7b93acc26b82a07
selehadin-cyber <sele4416@gmail.com>
2022-04-04 21:23:27 +0300

    social links added and fixed left

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import logo from "./bitmassssp.png";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import SocialList from "./components/Social";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="side">
+      <SocialList />
+      </div>
       <header className="App-header">
         <h2>Hi, my name is</h2>
-        <h1>Selehadin Abdurahman A.</h1>
-        <h3>i build meaning full websites</h3>
-        <p>
+        <h1 className="big-heading">Selehadin Abdurahman A.</h1>
+        <h3 className="big-heading">i build meaning full websites</h3>
+        <p className="hero-ptext">
           i am graduate from University of Pamukkale.i enjoy making cool and
           awesome sites and also making meaningfull and value delivering apps
         </p>
+        <a href="/" className="cta">Get in touch</a>
       </header>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./Navbar.css";
+import "./Navbar.scss";
 import Toggle from './Toggle';
 
 function Navbar() {
